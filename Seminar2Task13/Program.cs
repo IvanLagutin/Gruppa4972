617445972815725ebc834e4a0f2bc47d3a0d1745
Ivan <lagutin_ivan88@mail.ru>
2023-09-10 15:46:46 +0300

    Seminar 2 Task 13, 15

--- Seminar2Task13/Program.cs
+++ Seminar2Task13/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// №13 Напишите программу,
+// которая выводит третью цифру заданного числа или
+// сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите первое число");
+int ran = Convert.ToInt32(Console.ReadLine());
+char[] digitChar = ran.ToString().ToCharArray();
+System.Console.Write(digitChar[2]);
---
+++ Seminar2Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿// №15 
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool VlidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+int weekDay = Promt("Введите день недели >");
+if (VlidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Наконеу-то выходной");
+    }
+    else
+    {
+        Console.WriteLine("Придется поработать");
+    }
+}
