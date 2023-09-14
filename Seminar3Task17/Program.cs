2e9f29068e62cb4c07c4a913c710e2b1f4dc490a
Ivan <lagutin_ivan88@mail.ru>
2023-09-14 14:30:56 +0300

    Solution from seminar 3

---
+++ Seminar3Task17/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №17
+// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X не равен 0 и Y не равен 0 и выдает номер четверти плоскости, 
+// в которой находится эта точка.
+
+// 1) ввести X
+// 2) ввести Y
+// 3) поиск ответа 
+// 4) вывести результат
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+//По координатам точки определяем номер четверти
+int QurerTest(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 1;
+    if (x < 0 && y < 0) return 1;
+    if (x > 0 && y < 0) return 1;
+    return 0;
+}
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+int x = ReadData("Введите координату X: ");
+int y = ReadData("Введите координату Y: ");
+
+int res = QurerTest(x,y);
+PrintResult("Точка находится в четверти № "+res);
---
+++ Seminar3Task18/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №18
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// 1) ввести номер четверти
+// 2) ответить на вопрос
+// 3) вывести результат
+
+
+int getQurter()
+{
+    Console.Write("Введите номер четверти: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()?? "0");
+    return num;
+}
+
+string pointsIdent(int quarterNum)
+{
+    if (quarterNum == 1) return "Возможные значения: x>0, y>0";
+    if (quarterNum == 2) return "Возможные значения: x<0, y>0";
+    if (quarterNum == 3) return "Возможные значения: x<0, y<0";
+    if (quarterNum == 4) return "Возможные значения: x>0, y<0";
+    return "Неверное значени";
+}
+int quarterNum= getQurter();
+Console.WriteLine(pointsIdent(quarterNum));
